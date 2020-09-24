6390c8a8282200135125b9766c121e67eed4f241
Asim Cheema <asimcheema484@gmail.com>
2020-09-24 15:55:45 +0500

    theme change

--- resources/views/Product/index.blade.php
+++ resources/views/Product/index.blade.php
@@ -1,47 +1,50 @@
-@extends('layouts/master')
 
+@extends('layouts.master')
 @section('content')
+			
+<!-- Page Wrapper -->
+           
 
-<!-- basic table  -->
-                    <!-- ============================================================== -->
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <div class="card">
-                            <h5 class="card-header">Product Table</h5>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-striped table-bordered first">
-                                        <thead>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Name</th>
-                                                <th>Size</th>
-                                                <th>Price</th>
-                                                
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($products as $product)
-                                            <tr>
-                                            <td>{{$product->id}}</td>
-                                            <td>{{$product->product_name}}</td>
-                                            <td>{{$product->product_size}}</td>
-                                            <td>{{$product->product_price}}</td>
-                                                
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                        <tfoot>
-                                           
-                                        </tfoot>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
+					
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card mb-0">
+                
+                <div class="card-body">
+
+                    <div class="table-responsive">
+                        <table class="datatable table table-stripped mb-0 datatables">
+                            <thead>
+                                <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Size</th>
+                                <th>Price</th>                                
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($products as $product)
+                            <tr>
+                            <td>{{$product->id}}</td>
+                            <td>{{$product->product_name}}</td>
+                            <td>{{$product->product_size}}</td>
+                            <td>{{$product->product_price}}</td>
+                                
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                    <!-- ============================================================== -->
-                    <!-- end basic table  -->
-                    <!-- ============================================================== -->
                 </div>
-      
+            </div>
+        </div>
+    </div>
+				
+				
+			<!-- /Page Wrapper -->
+ @endsection
 
-@endsection
+		       
+
+	
