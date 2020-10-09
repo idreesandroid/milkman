e5739a085c2cfdd2fdec1a37c49c7a4f4d935c04
ZameerSandeelo <66078483+ZameerSandeelo@users.noreply.github.com>
2020-10-09 09:54:49 -0700

    payment request initiated.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,7 +9,7 @@ class PaymentController extends Controller
     //
     public function userList(Request $request)
     { 
-   $payments_g = "SELECT b.id , `name`    FROM `users` b 
+   $payments_g = "SELECT b.id , `name` FROM `users` b 
     INNER JOIN role_user c ON c.`user_id`=b.id WHERE c.`role_id`=5";
     $userList = DB::select($payments_g);
     return view('payment', compact('userList') ); 
@@ -28,6 +28,73 @@ class PaymentController extends Controller
     values ('$user_id','$payment_detail','$amount')";
      DB::insert($payment_ins);
     return redirect('payment')->with('msg','successfully paid'); 
+
+    }
+
+
+
+    public function payment_request(Request $request)
+    { 
+
+        $claim_amount =  $request->input('claim_amount'); 
+          $entry_by = session()->get('u_id');
+
+$chk_role_num = "SELECT a.id, c.`role_id` ,c.`role_title` 
+FROM users a
+INNER JOIN role_user b ON b.user_id=a.id
+INNER JOIN `roles` c ON c.id=b.`role_id`
+WHERE a.id = '$entry_by'";
+   $chk_role_num_records = DB::select($chk_role_num);
+
+ if(collect($chk_role_num_records)->first()) {
+    $result_current_role = json_decode(json_encode($chk_role_num_records[0]), true);
+    $current_role_id = $result_current_role['role_id'];
+
+$next_role = "SELECT MAX(role_id)next_role_id FROM roles WHERE role_id < $current_role_id ";
+$next_role_rs = DB::select($next_role);
+
+if(collect($next_role_rs)->first()) {
+    $next_role_result = json_decode(json_encode($next_role_rs[0]), true);
+    $next_role_id = $next_role_result['next_role_id'];
+}
+       
+  }
+
+ 
+
+  $request_data = array(         
+    'vendor_id'   => "$entry_by",
+    'claim_amount'   => "$claim_amount", 
+    'mark_to_role'   => "$next_role_id",
+    'mark_from_role'   => "$current_role_id" 
+    );
+    
+    
+  $request_id =  DB::table('payment_request')->insertGetId($request_data);
+
+  /*
+$ins_request = "INSERT INTO payment_request 
+(vendor_id, claim_amount, mark_to_role, mark_from_role)
+values ('$vendor_id','$claim_amount','$next_role_id','$current_role_id') ";
+$ins_request_record = DB::insert($ins_request);
+*/
+
+if($request_id>0 ){
+
+    $ins_request_detail = "INSERT INTO payment_request_comment 
+    (request_id, commnet_text, mark_to_role, mark_from_role,entry_by )
+    values ('$request_id','Please process my payment. Thanks','$next_role_id','$current_role_id','$entry_by') ";
+ DB::insert($ins_request_detail);
+ return redirect('/profile');
+
+
+ 
+}
+
+
+
+
+
 
     }
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -113,6 +113,10 @@ function printDiv(div_id)
 							</div>
 							<div class="noti-content">
 								<ul class="notification-list">
+									
+									
+									
+									
 									<li class="notification-message">
 										<a href="#">
 											<div class="media">
@@ -120,64 +124,25 @@ function printDiv(div_id)
 													<img alt="" src="{{asset('assets/img/profiles/avatar-02.jpg')}}">
 												</span>
 												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">John Doe</span> added new task <span class="noti-title">Patient appointment booking</span></p>
+
+													<p class="noti-details">
+													<span class="noti-title">John Doe</span>
+													 added new task 
+													 <span class="noti-title">Patient appointment booking</span>
+													 
+													 </p>
+													
+													
 													<p class="noti-time"><span class="notification-time">4 mins ago</span></p>
 												</div>
 											</div>
 										</a>
 									</li>
-									<li class="notification-message">
-										<a href="#">
-											<div class="media">
-												<span class="avatar">
-													<img alt="" src="{{asset('assets/img/profiles/avatar-03.jpg')}}">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
-													<p class="noti-time"><span class="notification-time">6 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="#">
-											<div class="media">
-												<span class="avatar">
-													<img alt="" src="{{asset('assets/img/profiles/avatar-06.jpg')}}">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
-													<p class="noti-time"><span class="notification-time">8 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="#">
-											<div class="media">
-												<span class="avatar">
-													<img alt="" src="{{asset('assets/img/profiles/avatar-17.jpg')}}">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
-													<p class="noti-time"><span class="notification-time">12 mins ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="notification-message">
-										<a href="#">
-											<div class="media">
-												<span class="avatar">
-													<img alt="" src="{{asset('assets/img/profiles/avatar-13.jpg')}}">
-												</span>
-												<div class="media-body">
-													<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
-													<p class="noti-time"><span class="notification-time">2 days ago</span></p>
-												</div>
-											</div>
-										</a>
-									</li>
+								 
+
+								 
+								 
+								 
 								</ul>
 							</div>
 							<div class="topnav-dropdown-footer">
