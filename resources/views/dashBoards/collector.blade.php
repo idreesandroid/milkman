ffb94b87ba5fdb6ea0f448fc1c875511ebbc060e
AsimShahbaz <37582290+AsimShahbaz@users.noreply.github.com>
2021-01-25 18:17:03 +0500

    permission applied

--- resources/views/dashBoards/collector.blade.php
+++ resources/views/dashBoards/collector.blade.php
@@ -11,4 +11,15 @@
     <!-- <a href="{{route('my.transaction')}}" class="card-link">My Transaction</a> -->
   </div>
 </div>	
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+<a href="#"> <h5 class="card-title">New Tasks</h5></a>
+  <ul>
+  <li><h2 class="card-title">Morning Task: {{$myMorningTask}}</h2></li>
+  <li><h2 class="card-title">Evening Task: {{$myEveningTask}}</h2></li>
+  </ul>
+    <!-- <a href="{{route('my.transaction')}}" class="card-link">My Transaction</a> -->
+  </div>
+</div>	
 @endsection
